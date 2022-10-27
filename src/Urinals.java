e70aa24b2643b20f52df19c3bce03a17f9cd7cbc
DSR <dollaking550@gmail.com>
2022-10-27 13:19:37 -0700

    added code skeleton

--- src/Urinals.java
+++ src/Urinals.java
@@ -2,6 +2,19 @@
 // GitHub: https://github.com/sathwikreddyd/ica8Testing
 
 public class Urinals {
+    public boolean goodString (String str) {
+        System.out.println("Not yet implemented");
+        return true;
+    }
+    public void openFile() {
+
+    }
+    public String getString() {
+        return "null";
+    }
+    public int countUrinals() {
+        return 0;
+    }
     public static void main(String[] args) {
 
     }
--- src/UrinalsTest.java
+++
@@ -1,5 +0,0 @@
-import org.junit.jupiter.api.*;
-
-public class UrinalsTest {
-    
-}
